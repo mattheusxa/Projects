5cb745e5719f1ff6d4ef0a0d20406ace17633c7c
mattheusxavier <mattheus.x.a@gmail.com>
2022-02-05 14:56:40 -0300

    Finish Splashpage create

--- Flutter/vakinha_burguer/lib/app/modules/splash/splash_page.dart
+++ Flutter/vakinha_burguer/lib/app/modules/splash/splash_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:vakinha_burguer/app/core/ui/widgets/vakinha_appbar.dart';
+import 'package:get/get_utils/get_utils.dart';
 import 'package:vakinha_burguer/app/core/ui/widgets/vakinha_button.dart';
-import 'package:vakinha_burguer/app/core/ui/widgets/vakinha_textformfield.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -9,23 +8,42 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: VakinhaAppbar(),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 20,
-          ),
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: VakinhaTextformfield(
-              label: 'Text Form',
+      body: Container(
+        color: const Color(0xFF140E0E),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                width: context.width,
+                child: Image.asset(
+                  'assets/images/lanche.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          VakinhaButton(label: 'Entrar', onPressed: () {}),
-        ],
+            Align(
+              alignment: Alignment.center,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: context.heightTransformer(reducedBy: 85),
+                  ),
+                  Image.asset('assets/images/logo.png'),
+                  const SizedBox(
+                    height: 60,
+                  ),
+                  VakinhaButton(
+                    label: 'ACESSAR',
+                    width: context.widthTransformer(reducedBy: 40),
+                    height: 35,
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
